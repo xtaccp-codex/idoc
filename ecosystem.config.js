1bb7598adb7d2567cc33b47c1b148eabc2c1f986
tao.xiang <tao.xiang@hougarden.com>
2026-02-26 12:25:20 +0800

    config: Update PM2 ecosystem configuration.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,9 @@ module.exports = {
     // 指向虚拟环境中的 python 解释器
     interpreter: "./venv_id_photo/bin/python",
     script: "api_server.py",
+    // 定时重启
+    // 分钟[0-59] 小时[0-23] 天[1-31] 月份[1-12] 星期数[0-6]
+    cron_restart: '0 4 * * *',
     // 注意: pm2 的 cluster 模式只支持 NodeJS，Python 深度多进程必须使用 fork。
     // 如果要多进程，推荐增加多个 app 配置或采用 Gunicorn。这里回退为 fork
     exec_mode: "fork",
